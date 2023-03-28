39e7250290d5d7bf993f247c88a8523732e7f82b
Artyem Lavrov <artyemlavrov39@gmail.com>
2023-03-28 21:40:06 +0300

    Tg & ctg

---
+++ src/main/java/ru/itmo/func/Ctg.java
@@ -0,0 +1,8 @@
+package ru.itmo.func;
+
+public class Ctg {
+
+    static double of(Double x) {
+        return Cos.of(x) / Sin.of(x);
+    }
+}
---
+++ src/main/java/ru/itmo/func/Tg.java
@@ -0,0 +1,8 @@
+package ru.itmo.func;
+
+public class Tg {
+
+    static Double of(Double x) {
+        return Sin.of(x) / Cos.of(x);
+    }
+}
