3c19fad4c426933b84dd8040190d5a7a86c850c4
Artem Dybov <artem.dybov@webium.team>
2023-03-28 21:16:27 +0300

    lab2

--- src/main/java/ru/itmo/func/Ln.java
+++ src/main/java/ru/itmo/func/Ln.java
@@ -2,6 +2,15 @@ package ru.itmo.func;
 
 public class Ln{
     public static Double of(Double x){
-        return x;
+        double result = 0;
+        double term = (x - 1) / x;
+        double numerator = term;
+
+        for (int i = 1; i <= 1000; i++) {
+            result += numerator / i;
+            numerator *= -term;
+        }
+
+        return result;
     }
 }
