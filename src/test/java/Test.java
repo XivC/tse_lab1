de9c9a0b7d969cfa0faa08b4dbc8d909a8eb9ad0
Artem Dybov <artem.dybov@webium.team>
2023-03-29 11:58:06 +0300

    seccsc

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,9 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import ru.itmo.func.*;
+
+import static org.mockito.Mockito.when;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class Test {
     MockedStatic<Cos> cos;
@@ -18,15 +21,7 @@ public class Test {
 
     @BeforeAll
     void doMock(){
-        this.cos = Mockito.mockStatic(Cos.class);
-        this.csc = Mockito.mockStatic(Csc.class);
-        this.ctg = Mockito.mockStatic(Ctg.class);
-        this.ln = Mockito.mockStatic(Ln.class);
-        this.log2 = Mockito.mockStatic(Log2.class);
-        this.log3 = Mockito.mockStatic(Log3.class);
-        this.sec = Mockito.mockStatic(Sec.class);
-        this.sin = Mockito.mockStatic(Sin.class);
-        this.tg = Mockito.mockStatic(Tg.class);
+
 
     }
 }
